81bc824e0eeca005e172832dea4ad2f2e08b108d
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-06-10 20:56:14 +0300

    10.06.25 20:56

--- src/dapes.ts
+++ src/dapes.ts
@@ -1,4 +1,5 @@
 import { Task } from "./task.ts";
 import { Group, Subgroup } from "./group.ts";
+import { start, startIfMain } from "./start.ts";
 
-export { Task, Group, Subgroup };
+export { Task, Group, Subgroup, start, startIfMain };
